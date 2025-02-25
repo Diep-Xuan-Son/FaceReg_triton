3475ed792a6add46ff2ce0d5c10292619db4f198
Diep-Xuan-Son <82936265+Diep-Xuan-Son@users.noreply.github.com>
2025-02-26 00:33:21 +0700

    test batch for retina and ghost face

--- my_repository/ghost_face_extract_feature/1/convert_tf2onnx.py
+++ my_repository/ghost_face_extract_feature/1/convert_tf2onnx.py
@@ -1,9 +1,29 @@
-import tensorflow as tf
-import tf2onnx
-import onnx
+# import tensorflow as tf
+# import tf2onnx
+# import onnx
 
-model = tf.saved_model.load("model.savedmodel")
-input_signature = [tf.TensorSpec([112, 112], tf.float32, name='x')]
-# Use from_function for tf functions
-onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature, opset=16)
-onnx.save(onnx_model, "model.onnx")
+# model = tf.saved_model.load("model.savedmodel")
+# input_signature = [tf.TensorSpec([112, 112], tf.float32, name='x')]
+# # Use from_function for tf functions
+# onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature, opset=16)
+# onnx.save(onnx_model, "model.onnx")
+
+
+
+
+
+#-----------test----------
+import onnxruntime
+import numpy as np
+import time
+
+batch_size = 10
+x = np.random.rand(batch_size, 112, 112, 3).astype(np.float32)
+
+st_time = time.time()
+# for _ in range(10):
+ort_session = onnxruntime.InferenceSession("model.onnx", providers=["CPUExecutionProvider"])
+ort_inputs = {ort_session.get_inputs()[0].name: x}
+ort_outs = ort_session.run(None, ort_inputs)
+print(ort_outs[0].shape)
+print(f"----Duration: {time.time() - st_time}")
